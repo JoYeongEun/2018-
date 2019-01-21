805741ef87b238b1ed3a7c1e5b1c9c16864212bd
yeongeun <duddmd0219@naver.com>
2019-01-21 21:02:29 +0900

    subtle change

--- djangoproject/wordcnt/views.py
+++ djangoproject/wordcnt/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from konlpy.tag import Twitter
+from collections import Counter
+
 # Create your views here.
 def home(reqeust):
     return render(reqeust,'home.html')
@@ -9,12 +12,18 @@ def about(reqeust):
 def result(reqeust):
     text = reqeust.GET['fulltext']
     words = text.split()
+
+    nlpy = Twitter()
+    nouns = nlpy.nouns(words)
+
+    count = Counter(nouns) 
+
     word_dictionary = {}
 
-    for word in words:
+    for word in nouns:
         if word in word_dictionary:
             word_dictionary[word]+=1
         else :
             word_dictionary[word]=1
 
-    return render(reqeust,'result.html',{'full':text,'total':len(words),'dictionary':word_dictionary.items()})
+    return render(reqeust,'result.html',{'full':text,'total':len(nouns),'dictionary':word_dictionary.items()})
